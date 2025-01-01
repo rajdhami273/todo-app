34425c79d1a60dae924c420f14bc1b04b26da8cd
Rajendra Dhami <rajendra@Apollo-Rajendra-Dhamis-Mac.local>
2025-01-01 17:35:25 +0530

    Removed credentials

--- backend/config.js
+++ backend/config.js
@@ -1,5 +1,4 @@
 module.exports = {
-  mongoDBConnectionString:
-    "mongodb+srv://rajendra:rajendra123@cluster0.qgkwp.mongodb.net/todoapp?retryWrites=true&w=majority&appName=Cluster0",
-  jwtSecretKey: "my-blogpost-app-123123123",
+  mongoDBConnectionString: "<your_db_address",
+  jwtSecretKey: "<your_secret_key>",
 };
